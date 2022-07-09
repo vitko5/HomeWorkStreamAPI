a6028ec1229fe60ea9e612c5066f8ba46e12d9fa
vitko5 <cp.advert0@googlemail.com>
2022-07-09 20:21:36 +0300

    Update all to stream

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.OptionalDouble;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -27,96 +28,70 @@ public class Main {
         //}
 
         //1. Вибрати чоловіків-військовозобов'язаних (від 18 до 27 років)
-        for (People people : peopleList) {
-            if (people.getSex() == 'M' && (people.getAge() >= 18 && people.getAge() <= 27)) {
-                System.out.printf("%s (%s y.o.) voenoobyazanniy\n",people.getName(),people.getAge());
-            }
-        }
+        peopleList.stream()
+                .filter(people -> people.getSex() == 'M' && people.getAge() >= 18 && people.getAge() <= 27)
+                .forEach(people -> System.out.println(people.getName()));
         System.out.println("===========================================");
 
         //2. Знайти середній вік серед чоловіків
-        int ageSum = 0;
-        int ageCount = 0;
-        for (People people : peopleList) {
-            if (people.getSex() == 'M') {
-                ageSum += people.getAge();
-                ageCount++;
-            }
-        }
-        System.out.println("Average age from all male people is: " + ageSum/ageCount);
+        double average = peopleList.stream()
+                .filter(people -> people.getSex() == 'M')
+                .mapToDouble(People::getAge)
+                .average()
+                .getAsDouble();
+        System.out.println("Average age from all male people is: " + average);
         System.out.println("===========================================");
 
         //3. Знайти кількість потенційно працездатних людей у вибірці (тобто від 18 років і з огляду на що жінки виходять в 55 років, а чоловік в 60)
-        for (People people : peopleList) {
-            if (people.getAge() >= 18 && ((people.getSex() == 'F' && people.getAge()<55) || (people.getSex() == 'M' && people.getAge()<60))) {
-                System.out.printf("%s (%s) is good for work\n",people.getName(),people.getAge());
-            }
-        }
+        peopleList.stream()
+                .filter(people -> people.getAge() >= 18 && ((people.getSex() == 'M' && people.getAge() <= 60) || (people.getSex() == 'F' && people.getAge() <= 55)))
+                .forEach(people -> System.out.println(people.getName()));
         System.out.println("===========================================");
 
         //4. Відсортувати колекцію людей за ім'ям в зворотному алфавітному порядку
-        List<People> sortedPeopleSet = peopleList.stream().sorted(Comparator.comparing(People::getName).reversed()).toList();
-        for (People people : sortedPeopleSet) {
-            System.out.println(people.getName());
-        }
+        peopleList.stream()
+                .sorted(Comparator.comparing(People::getName).reversed())
+                .forEach(people -> System.out.println(people.getName()));
         System.out.println("===========================================");
 
         //5. Відсортувати колекцію людей спочатку за ім’ям, а потім за віком
-        Comparator<People> comparator = Comparator.comparing(People::getName).thenComparing(People::getAge);
-        List<People> sorted2PeopleList = peopleList.stream().sorted(comparator).toList();
-        for (People people : sorted2PeopleList) {
-            System.out.printf("%s (%s)\n",people.getName(),people.getAge());
-        }
+        peopleList.stream()
+                .sorted(Comparator.comparing(People::getName).thenComparing(People::getAge))
+                .forEach(people -> System.out.println(people.getName() + " " + people.getAge()));
         System.out.println("===========================================");
 
         //6. Знайти найстаршу людину
-        People oldestPeople = null;
-        for (People people : peopleList) {
-            if (oldestPeople == null) {
-                oldestPeople = people;
-            } else if (oldestPeople.getAge() < people.getAge()){
-                oldestPeople = people;
-            }
-        }
+        People oldestPeople = peopleList.stream()
+                .max(Comparator.comparing(People::getAge))
+                .get();
         System.out.printf("%s (%s) is oldest people from database\n",oldestPeople.getName(),oldestPeople.getAge());
         System.out.println("===========================================");
 
         //7. Знайти наймолодшу людину
-        People youngerPeople = null;
-        for (People people : peopleList) {
-            if (youngerPeople == null) {
-                youngerPeople = people;
-            } else if (youngerPeople.getAge() > people.getAge()){
-                youngerPeople = people;
-            }
-        }
+        People youngerPeople  = peopleList.stream()
+                .min(Comparator.comparing(People::getAge))
+                .get();
         System.out.printf("%s (%s) is younger people from database\n",youngerPeople.getName(),youngerPeople.getAge());
         System.out.println("===========================================");
 
         //8. Вивести скільки є чоловіків
-        int countMale = 0;
-        for (People people : peopleList) {
-            if (people.getSex() == 'M') countMale++;
-        }
+        long countMale = peopleList.stream()
+                .filter(people -> people.getSex() == 'M')
+                .count();
         System.out.printf("Database have %s male\n",countMale);
         System.out.println("===========================================");
 
         //9. Вивести скільки є жінок
-        int countFemale = 0;
-        for (People people : peopleList) {
-            if (people.getSex() == 'F') countFemale++;
-        }
+        long countFemale = peopleList.stream()
+                .filter(people -> people.getSex() == 'F')
+                .count();
         System.out.printf("Database have %s female\n",countFemale);
         System.out.println("===========================================");
 
         //10. Вивеcти всіх жінок в яких ім’я починається на “A”
-        Pattern regex = Pattern.compile("^A.+");
-        for (People people : peopleList) {
-            Matcher matcher = regex.matcher(people.getName());
-            if (matcher.matches()) {
-                System.out.printf("%s is starting from A\n",people.getName());
-            }
-        }
+        peopleList.stream()
+                .filter(people -> people.getName().startsWith("A"))
+                .forEach(people -> System.out.println(people.getName()));
         System.out.println("===========================================");
     }
 }
